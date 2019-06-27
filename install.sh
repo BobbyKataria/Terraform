aebbf7ee2e99d8c9052ea3ef8200d7131eabf69a
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-06-27 10:11:00 +0100

    First commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+
+
+sudo apt get upgrade
+
+sudo apt upgrade -y
+
+sudo apt install -y unzip wget
+
+wget https://releases.hashicorp.com/terraform/0.12.3/terraform_0.12.3_linux_amd64.zip
+
+unzip terraform_*_linux_*.zip
+
+sudo mv terraform /usr/local/bin
+
+rm terraform_*_linux_*.zip
+
+terraform --version
